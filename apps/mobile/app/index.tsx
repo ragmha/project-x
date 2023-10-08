6f070e6d48144fcd934cb1e6982db2ab9a6e6719
Räghib Hasan <raghibmidhat@gmail.com>
2023-10-08 08:58:13 +0300

    extract health data to hook

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,29 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { useCallback, useState } from 'react';
+import { RefreshControl, SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 
 import RingProgress from '../components/RingProgress';
 import Value from '../components/Value';
+import { useHealthData } from '../hooks/use-health-data';
+
+const STEPS_GOAL = 10000;
 
 export default function App() {
+  const todaysDate = new Date();
+  const [refreshing, setRefreshing] = useState(false);
+  const { steps, distance, flights, refetch } = useHealthData(todaysDate);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    refetch().finally(() => setRefreshing(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <RingProgress radius={150} strokeWidth={50} progress={0.5} />
-      <View style={styles.values}>
-        <Value label="Steps" value="1219" />
-        <Value label="Distance" value="0,75km" />
-        <Value label="Flights Climbed" value="12" />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      >
+        <RingProgress radius={150} strokeWidth={40} progress={steps / STEPS_GOAL} />
+        <View style={styles.values}>
+          <Value label="Goal" value={STEPS_GOAL.toString()} />
+          <Value label="Steps" value={steps.toString()} />
+          <Value label="Distance" value={`${(distance / 1000).toFixed(2)}km`} />
+          <Value label="Flights Climbed" value={flights.toString()} />
+        </View>
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    justifyContent: 'center',
-    padding: 12,
   },
-  values: { flexDirection: 'row', gap: 25, flexWrap: 'wrap', marginTop: 50 },
+  scrollView: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  values: { flexDirection: 'row', gap: 25, flexWrap: 'wrap', marginTop: 100 },
 });
---
+++ apps/mobile/hooks/use-health-data.ts
@@ -0,0 +1,131 @@
+import { useState, useEffect, useCallback } from 'react';
+import AppleHealthKit, { HealthInputOptions, HealthKitPermissions } from 'react-native-health';
+
+import supabase from '../lib/supabase';
+
+const permissions: HealthKitPermissions = {
+  permissions: {
+    read: [
+      AppleHealthKit.Constants.Permissions.Steps,
+      AppleHealthKit.Constants.Permissions.FlightsClimbed,
+      AppleHealthKit.Constants.Permissions.DistanceWalkingRunning,
+    ],
+    write: [],
+  },
+};
+
+type UseHealthData = {
+  steps: number;
+  flights: number;
+  distance: number;
+  refetch: () => Promise<void>;
+};
+
+export const useHealthData = (date: Date): UseHealthData => {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [steps, setSteps] = useState(0);
+  const [flights, setFlights] = useState(0);
+  const [distance, setDistance] = useState(0);
+
+  useEffect(() => {
+    AppleHealthKit.isAvailable((err, isAvailable) => {
+      if (err) {
+        console.error(`Error checking availability: ${err}`);
+        return;
+      }
+
+      if (!isAvailable) {
+        console.error('HealthKit is not available');
+        return;
+      }
+
+      AppleHealthKit.initHealthKit(permissions, (error) => {
+        if (error) {
+          console.error(`Error getting permission: ${error}`);
+          return;
+        }
+
+        setHasPermission(true);
+      });
+    });
+  }, []);
+
+  const fetchData = useCallback(async () => {
+    if (!hasPermission) {
+      return;
+    }
+
+    const options: HealthInputOptions = {
+      date: date.toISOString(),
+      includeManuallyAdded: false,
+    };
+
+    try {
+      const stepCount = await new Promise<number>((resolve, reject) => {
+        AppleHealthKit.getStepCount(options, (error, results) => {
+          if (error) {
+            reject(new Error(`Error getting steps count: ${error}`));
+          } else {
+            resolve(results.value);
+          }
+        });
+      });
+
+      const flightsCount = await new Promise<number>((resolve, reject) => {
+        AppleHealthKit.getFlightsClimbed(options, (error, results) => {
+          if (error) {
+            reject(new Error(`Error getting flights count: ${error}`));
+          } else {
+            resolve(results.value);
+          }
+        });
+      });
+
+      const distanceCount = await new Promise<number>((resolve, reject) => {
+        AppleHealthKit.getDistanceWalkingRunning(options, (error, results) => {
+          if (error) {
+            reject(new Error(`Error getting distance count: ${error}`));
+          } else {
+            resolve(results.value);
+          }
+        });
+      });
+
+      setSteps(stepCount);
+      setFlights(flightsCount);
+      setDistance(distanceCount);
+
+      const { data, error } = await supabase.from('health_data').upsert([
+        {
+          date: new Date().toISOString(),
+          steps: stepCount,
+          flights: flightsCount,
+          distance: distanceCount,
+        },
+      ]);
+
+      if (error) {
+        console.error(`Error pushing data to Supabase: ${error.message}`);
+      } else {
+        console.log(`Data pushed to Supabase: ${JSON.stringify(data)}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [hasPermission]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const refetch = async () => {
+    await fetchData();
+  };
+
+  return {
+    steps,
+    flights,
+    distance,
+    refetch,
+  };
+};
